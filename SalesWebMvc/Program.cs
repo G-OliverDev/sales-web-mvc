b8af61de04678d75b7b8aca6fa6d087a15943258
G-OliverDev <180020076+G-OliverDev@users.noreply.github.com>
2025-08-18 10:36:04 -0300

    refactor: added Startup.cs configuration into Program.cs

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -9,18 +9,28 @@ namespace SalesWebMvc
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
